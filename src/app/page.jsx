3e3be7ec6ff308b3eb68159968d6facce3423253
mohamedtarek77 <mtarek7878@gmail.com>
2023-12-27 16:06:57 +0400

    second

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ async function getData() {
   try {
 
     // const res = await fetch("http://localhost:3000/api/posts/getposts");
-    const res = await fetch("https://taskstarek123.vercel.app/api/posts/getposts");
+    const res = await fetch("https://tasks-eight-rosy.vercel.app/api/posts/getposts");
 
     // const res = await fetch(`${process.env.DOMAIN}/api/posts/getposts`);
 
