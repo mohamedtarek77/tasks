2930865c3d2bc2ffda7be07bb05a3708bf93b7f9
mohamedtarek77 <mtarek7878@gmail.com>
2023-12-27 16:18:29 +0400

    5th

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,51 +1,55 @@
 
-import React from "react";
-import Post from "../components/Post"
-import AddTaskBtn from "../components/AddTaskBtn"
+// import React from "react";
+// import Post from "../components/Post"
+// import AddTaskBtn from "../components/AddTaskBtn"
 
-async function getData() {
-  try {
+// async function getData() {
+//   try {
 
-    // const res = await fetch("http://localhost:3000/api/posts/getposts");
-    const res = await fetch("https://tasks-eight-rosy.vercel.app/api/posts/getposts");
+//     // const res = await fetch("http://localhost:3000/api/posts/getposts");
+//     const res = await fetch("https://tasks-eight-rosy.vercel.app/api/posts/getposts");
 
-    // const res = await fetch(`${process.env.DOMAIN}/api/posts/getposts`);
+//     // const res = await fetch(`${process.env.DOMAIN}/api/posts/getposts`);
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
+//     if (!res.ok) {
+//       throw new Error("Failed to fetch data");
+//     }
 
-    return res.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return null;
-  }
-}
+//     return res.json();
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return null;
+//   }
+// }
 
-export default async function Page() {
-  const data = await getData()
-  console.log(data)
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800 w-full border border-gray-300 border-2  ">
-      <>
+// export default async function Page() {
+//   const data = await getData()
+//   console.log(data)
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800 w-full border border-gray-300 border-2  ">
+//       <>
 
-      <div className="flex flex-wrap">
-            <AddTaskBtn />
+//       <div className="flex flex-wrap">
+//             <AddTaskBtn />
 
-            {
-                data?.data?.map((post) => (
-                    <Post
-                        key={post._id}
-                        post={post}
+//             {
+//                 data?.data?.map((post) => (
+//                     <Post
+//                         key={post._id}
+//                         post={post}
                 
-                    />
-                ))}
-        </div>
-      </>
-    </main>
+//                     />
+//                 ))}
+//         </div>
+//       </>
+//     </main>
 
-  );
-}
+//   );
+// }
+
+
+
+
 
 
 
@@ -104,3 +108,44 @@ export default async function Page() {
 //   );
 // }
 
+
+
+
+
+
+
+
+import React from "react";
+import Post from "../components/Post"
+import AddTaskBtn from "../components/AddTaskBtn"
+
+export async function getStaticProps() {
+  const res = await fetch("https://tasks-eight-rosy.vercel.app/api/posts/getposts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+
+  return {
+    props: {
+      posts: data.data,
+    },
+    revalidate: 60, // revalidate every 60 seconds
+  };
+}
+
+export default function Page({ posts }) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-800 w-full border border-gray-300 border-2  ">
+      <div className="flex flex-wrap">
+        <AddTaskBtn />
+
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </div>
+    </main>
+  );
+}
